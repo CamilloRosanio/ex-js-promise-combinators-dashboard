4823a7011078aa1dedd5e8667d658598653f7a02
MyUsername <rosanio.c@gmail.com>
2025-03-05 10:25:33 +0100

    Bonus 2

--- js/script.js
+++ js/script.js
@@ -9,6 +9,36 @@ async function fetchJson(url) {
 
 }
 
+// EX. + Bonus 1
+// async function getDashboardData(city) {
+
+//     try {
+//         console.log('Caricando la query per:', city)
+//         const destinationPromise = fetchJson(`https://boolean-spec-frontend.vercel.app/freetestapi/destinations?search=${city}`);
+//         const weathersPromise = fetchJson(`https://boolean-spec-frontend.vercel.app/freetestapi/weathers?search=${city}`);
+//         const airportsPromise = fetchJson(`https://boolean-spec-frontend.vercel.app/freetestapi/airports?search=${city}`);
+
+//         const promises = [destinationPromise, weathersPromise, airportsPromise];
+//         const [destinations, weathers, airports] = await Promise.all(promises);
+
+//         const destination = destinations[0];
+//         const weather = weathers[0];
+//         const airport = airports[0];
+
+//         return {
+//             city: destination ? destinations[0].name : null,
+//             country: destination ? destinations[0].country : null,
+//             temperature: weather ? weathers[0].temperature : null,
+//             weather: weather ? weathers[0].weather_description : null,
+//             airport: airport ? airports[0].name : null,
+//         }
+//     } catch (error) {
+//         throw new Error('Errore durante la fetch:', error.message);
+//     }
+// }
+
+
+// Bonus 2
 async function getDashboardData(city) {
 
     try {
@@ -18,19 +48,39 @@ async function getDashboardData(city) {
         const airportsPromise = fetchJson(`https://boolean-spec-frontend.vercel.app/freetestapi/airports?search=${city}`);
 
         const promises = [destinationPromise, weathersPromise, airportsPromise];
-        const [destinations, weathers, airports] = await Promise.all(promises);
+        const [destinationsResult, weathersResult, airportsResult] = await Promise.allSettled(promises);
 
-        const destination = destinations[0];
-        const weather = weathers[0];
-        const airport = airports[0];
+        const data = {};
 
-        return {
-            city: destination ? destinations[0].name : null,
-            country: destination ? destinations[0].country : null,
-            temperature: weather ? weathers[0].temperature : null,
-            weather: weather ? weathers[0].weather_description : null,
-            airport: airport ? airports[0].name : null,
+        if (destinationsResult.status === 'rejected') {
+            console.error('Problema in destinations:', destinationsResult.reason)
+            data.city = null;
+            data.country = null;
+        } else {
+            const destination = destinationsResult.value[0];
+            data.city = destination ? destination.name : null;
+            data.country = destination ? destination.country : null;
         }
+
+        if (weathersResult.status === 'rejected') {
+            console.error('Problema in weathers:', weathersResult.reason)
+            data.temperature = null;
+            data.weather = null;
+        } else {
+            const weather = weathersResult.value[0];
+            data.temperature = weather ? weather.temperature : null;
+            data.weather = weather ? weather.weather_description : null;
+        }
+
+        if (airportsResult.status === 'rejected') {
+            console.error('Problema in weathers:', airportsResult.reason)
+            data.airport = null;
+        } else {
+            const airport = airportsResult.value[0];
+            data.airport = airport ? airport.name : null;
+        }
+
+        return data;
     } catch (error) {
         throw new Error('Errore durante la fetch:', error.message);
     }
