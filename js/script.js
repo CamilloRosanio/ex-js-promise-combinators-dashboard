5a2e6c08d73f927050b90223be5a89aeb41cadfc
MyUsername <rosanio.c@gmail.com>
2025-03-05 10:05:07 +0100

    DASHBOARD DELLA CITTA'

--- js/script.js
+++ js/script.js
@@ -1,4 +1,39 @@
 /***********************************************************************
-# SNACK 1
+# DASHBOARD DELLA CITTA'
 ***********************************************************************/
 
+async function fetchJson(url) {
+    const response = await fetch(url);
+    const object = await response.json();
+    return object;
+
+}
+
+async function getDashboardData(city) {
+
+    try {
+        console.log('Caricando la query per:', city)
+        const destinationPromise = fetchJson(`https://boolean-spec-frontend.vercel.app/freetestapi/destinations?search=${city}`);
+        console.log(destinationPromise);
+        const weathersPromise = fetchJson(`https://boolean-spec-frontend.vercel.app/freetestapi/weathers?search=${city}`);
+        console.log(weathersPromise);
+        const airportsPromise = fetchJson(`https://boolean-spec-frontend.vercel.app/freetestapi/airports?search=${city}`);
+        console.log(airportsPromise);
+
+        const promises = [destinationPromise, weathersPromise, airportsPromise];
+        const [destinations, weathers, airports] = await Promise.all(promises);
+
+        return {
+            city: destinations[0].name,
+            country: destinations[0].country,
+            temperature: weathers[0].temperature,
+            weather: weathers[0].weather_description,
+            airport: airports[0].name,
+        }
+    } catch (error) {
+        throw new Error('Errore durante la fetch:', error.message);
+    }
+}
+
+const finalResult = getDashboardData('london');
+console.log(finalResult);
